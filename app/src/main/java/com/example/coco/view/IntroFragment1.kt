bf82b1501bb3ace595df0756e7134804eb3c3338
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-19 21:54:38 +0900

    [FEAT] : 인트로 Navigation 완료

--- app/src/main/java/com/example/coco/view/IntroFragment1.kt
+++ app/src/main/java/com/example/coco/view/IntroFragment1.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.coco.R
+import com.example.coco.databinding.FragmentIntro1Binding
 
+
+// findViewByID -> ViewBinding
 
 class IntroFragment1 : Fragment() {
+
+    // viewBinding 쓰기
+    private var _binding : FragmentIntro1Binding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +27,23 @@ class IntroFragment1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro1, container, false)
+        _binding = FragmentIntro1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextBtn.setOnClickListener {
+            // main_nav의 intro 1 -> intro 2 로 이동하는 action 사용
+            Navigation.findNavController(view).navigate(R.id.action_introFragment1_to_introFragment2)
+        }
+    }
+
+    // Fragment가 없어졌을 때 처리
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
