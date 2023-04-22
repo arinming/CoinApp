0fb2b1b4a63b9b6a82253a6cf2b11f11b03f82bb
아딘컴\김아린 <jyandysys1@naver.com>
2023-04-22 22:47:50 +0900

    [FEAT] : Select 액티비티로 이동

--- app/src/main/java/com/example/coco/view/IntroFragment2.kt
+++ app/src/main/java/com/example/coco/view/IntroFragment2.kt
@@ -1,14 +1,19 @@
 package com.example.coco.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.coco.R
+import com.example.coco.databinding.FragmentIntro2Binding
 
 
 class IntroFragment2 : Fragment() {
+
+    private var _binding : FragmentIntro2Binding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +23,23 @@ class IntroFragment2 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro2, container, false)
+
+        _binding = FragmentIntro2Binding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextBtn.setOnClickListener {
+            val intent = Intent(requireActivity(), SelectActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
